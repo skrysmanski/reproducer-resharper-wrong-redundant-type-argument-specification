8afcab0150a452e1852836d706bf2cfab02c1505
Sebastian Krysmanski <noreply@manski.net>
2021-01-19 12:23:12 +0100

    Added reproducer

--- ReSharperReproducer/Reproducer.cs
+++ ReSharperReproducer/Reproducer.cs
@@ -8,7 +8,14 @@ namespace ReSharperReproducer
     {
         public void ReproducerMethod()
         {
+            // ReSharper disable UnusedParameter.Local
+            static void RunTest<T>(T notNullValue, T nullValue)
+            // ReSharper restore UnusedParameter.Local
+            {
+            }
 
+            RunTest<string?>("abc", null);
+            //RunTest("abc", null); // creates: CS8625: Cannot convert null literal to non-nullable reference type.
         }
     }
 }
