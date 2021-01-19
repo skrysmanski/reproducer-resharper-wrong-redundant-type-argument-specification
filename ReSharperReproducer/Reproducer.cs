e7c66ba9a215d2f4e43c73d14f3b8f9869b808a3
Sebastian Krysmanski <noreply@manski.net>
2021-01-19 12:08:32 +0100

    Added repository skeleton

---
+++ ReSharperReproducer/Reproducer.cs
@@ -0,0 +1,14 @@
+﻿//
+// This file contains a reproducer for a ReSharper bug.
+//
+
+namespace ReSharperReproducer
+{
+    public class Reproducer
+    {
+        public void ReproducerMethod()
+        {
+
+        }
+    }
+}
